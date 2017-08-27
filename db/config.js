901456f82712b00bf3794ecb500dd68dc960f4fd
Kiwilicious <jhwang137@gmail.com>
2017-08-27 14:35:27 -0700

    Removed comment

--- db/config.js
+++ db/config.js
@@ -3,8 +3,6 @@ const path = require('path');
 const dbURL = require(path.resolve(__dirname, '../env/config')).dbURL;
 
 const db = new Sequelize(dbURL, {
-  // For RDS/AWS the dbURL should be in the following format
-  // 'postgres://userNameHere:passwordHere@endpointHere:portHere/dbNameHere'
   dialect: 'postgres',
   pool: {max: 5, min: 0, idle: 10000}
 })
