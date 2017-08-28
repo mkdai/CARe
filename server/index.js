1b83faf338f2a39690ae80f08603fa4b7bf7595a
Ethan Fung <EthanEFung@gmail.com>
2017-08-28 14:21:56 -0700

    npm install timekit-sdk

--- server/index.js
+++ server/index.js
@@ -5,15 +5,16 @@ const express = require('express');
 const models = require('../db/index');
 const path = require('path');
 const routes = require('./routes/index');
+const timekit = require('timekit-sdk');
 
-const PORT = process.env.PORT || 3000;
+const PORT =  process.env.PORT || 3000;
 
 const app = express()
   .use(cors())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(express.static(path.resolve(__dirname, '../client/public')))
-  .use('/api', routes);  
+  .use('/api', routes)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'))
