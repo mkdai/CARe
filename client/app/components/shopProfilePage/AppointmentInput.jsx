e49afac4fb80fedffef5c578d2e126a6e0c6b4bb
Ethan Fung <EthanEFung@gmail.com>
2017-08-29 12:01:07 -0700

    Render AppointmentInput with select Form control

--- client/app/components/shopProfilePage/AppointmentInput.jsx
+++ client/app/components/shopProfilePage/AppointmentInput.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import {
+  Form, 
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from 'react-bootstrap';
 
 class AppointmentInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dates: [],
+      times: []
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
   render() {
     return (
-      <div>
-        I am the appointment input
-      </div>
+      <Form>
+      <FormGroup controlId="formControlsSelect">
+        <ControlLabel>Select</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="select">select</option>
+          <option value="other">...</option>
+        </FormControl>
+      </FormGroup>
+      </Form>
     );
   }
 }
