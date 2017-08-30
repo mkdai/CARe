3a1e4fd722c27b376c49df3c124f4e503568bfe9
Ethan Fung <EthanEFung@gmail.com>
2017-08-30 14:08:30 -0700

    Create ShopDashboard Container

---
+++ client/app/containers/shopDashboard/ShopDashboard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class ShopDashboard extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ShopDashboard;
+
+// Hi Ethan,
+
+// Booking.js is currently only for making the bookings, i.e. the customers' point of view.
+
+// Although there's no official support for what you're requesting, it shouldn't be too hard to develop yourself - here's how I'd go about it:
+// Use our JS SDK (https://github.com/timekit-io/js-sdk) for fetching the bookings for a given resource.
+// Use the FullCalendar (http://fullcalendar.io) library, which also is the main component in booking.js, to render the bookings in a calendar view.
+// Let me know if you have any luck with it 👍
