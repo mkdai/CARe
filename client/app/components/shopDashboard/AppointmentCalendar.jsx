d8d02b3046423f853ee7ea785eb909844d509794
Ethan Fung <EthanEFung@gmail.com>
2017-08-31 15:57:09 -0700

    Rid "title" of shop dashboard appointment calendar

--- client/app/components/shopDashboard/AppointmentCalendar.jsx
+++ client/app/components/shopDashboard/AppointmentCalendar.jsx
@@ -13,10 +13,6 @@ class AppointmentCalendar extends Component {
   }
 
   componentDidMount() {
-    let today = new Date(2017, 7, 31).getTime();
-    // console.log("today is: ", today, "and it is a ", typeof today);
-    let tmrw = new Date(2017, 8, 30).getTime();
-
     timekit.configure({
       app: "hack-reactor-124",
       inputTimestampFormat: "U",
@@ -26,25 +22,10 @@ class AppointmentCalendar extends Component {
     timekit
       .auth({ email: timekitEmail, password: timekitPassword })
       .then(() => timekit.include("attributes").getBookings())
-      // .then(() =>
-      //   timekit.getEvents({
-      //     start: today,
-      //     end: tmrw
-      //   })
-      // )
-      // .then(
-      //   timekit.findTime({
-      //     calendar_ids: ["9aefc3b5-f55b-4f41-afd2-ccb2829fdfc8"],
-      //   })
-      // )
       .then(res => {
         let bookings = [];
         res.data.forEach(booking => {
           if (!booking.completed && booking.state === "confirmed") {
-            console.log(
-              "these are the attributes of each booking",
-              booking.attributes
-            );
             let { start, end, what } = booking.attributes.event;
             let title = what;
             bookings.push({ start, end, title });
@@ -55,10 +36,6 @@ class AppointmentCalendar extends Component {
         );
       })
       .then(() => {
-        console.log(
-          "this is the state before setting events on calendar",
-          this.state
-        );
         $("#calendar").fullCalendar({
           events: this.state.bookings,
           defaultView: "listWeek"
@@ -69,7 +46,6 @@ class AppointmentCalendar extends Component {
   render() {
     return (
       <div>
-        <h1>I am the appointment Calendar</h1>
         <div id="calendar" />
       </div>
     );
