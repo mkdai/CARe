1b580c3b2c855e87306f74cf3e6222e13c864553
Kiwilicious <jhwang137@gmail.com>
2017-09-01 15:02:32 -0700

    Added framework for user car history

---
+++ client/app/containers/userHistory/MaintenanceEntry.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class MaintenanceEntry extends Component {
+  render() {
+    return (
+      <div>
+        <div>Shop: Generic Car Shop</div>
+        <div>Service Description: Oil Change</div>
+        <div>
+          Additional Notes: There was an oil leak in the engine. Go cuck
+          yourself
+        </div>
+      </div>
+    );
+  }
+}
--- client/app/containers/userHistory/UserHistory.jsx
+++ client/app/containers/userHistory/UserHistory.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, ListGroup } from "react-bootstrap";
 import UserHistoryEntry from "./UserHistoryEntry.jsx";
 
 export default class UserHistory extends Component {
   render() {
     return (
       <Grid>
-        <UserHistoryEntry />
+        <ListGroup>
+          <UserHistoryEntry />
+          <UserHistoryEntry />
+          <UserHistoryEntry />
+          <UserHistoryEntry />
+        </ListGroup>
       </Grid>
     );
   }
