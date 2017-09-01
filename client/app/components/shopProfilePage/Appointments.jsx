c7426fe6e74c5867a95e0e1d555c3951a5438aad
Ethan Fung <EthanEFung@gmail.com>
2017-09-01 15:13:05 -0700

    Readying for merge

--- client/app/components/shopProfilePage/Appointments.jsx
+++ client/app/components/shopProfilePage/Appointments.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { Button, Grid, Collapse, Well } from "react-bootstrap";
 import AppointmentInput from "./AppointmentInput";
 import AppointmentsList from "./AppointmentsList";
 import timekit from "timekit-sdk";
 import { timekitEmail, timekitPassword } from "../../../../env/config";
+import TimekitBooking from "timekit-booking";
 
 class Appointments extends Component {
   constructor(props) {
@@ -12,7 +14,9 @@ class Appointments extends Component {
       service: "",
       date: new Date().toISOString(),
       times: [],
-      time: ""
+      time: new Date().toISOString(),
+      shopCalendar: "9aefc3b5-f55b-4f41-afd2-ccb2829fdfc8",
+      openList: false
     };
     this.handleServiceChange = this.handleServiceChange.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
@@ -29,12 +33,12 @@ class Appointments extends Component {
       .auth({ email: timekitEmail, password: timekitPassword })
       .then(() =>
         timekit.findTime({
-          calendar_ids: ["9aefc3b5-f55b-4f41-afd2-ccb2829fdfc8"],
+          calendar_ids: [this.state.shopCalendar],
           future: "12 hours",
           // filters: {
           //   and: [{ specific_time: { start: 9, end: 24 } }]
           // },
-          length: "1 Hour"
+          length: "30 minutes"
         })
       )
       .then(res => {
@@ -70,20 +74,48 @@ class Appointments extends Component {
     let time = e.target.value;
     let { services, service, dates, date, times } = this.state;
 
-    this.setState({ services, service, dates, date, times, time });
+    this.setState({ time }, () =>
+      console.log("this is the state after handletime", this.state)
+    );
   }
 
   handleDateChange(date, formattedDate) {
-    let { services, service, dates, times, time } = this.state;
-
-    this.setState({ services, service, dates, date, times, time }, () =>
-      console.log(this.state, "this is the state of appointments")
-    );
+    this.setState({ date });
   }
 
   handleFindApptClick(e) {
     e.preventDefault();
-    console.log("i hear you");
+    let { time, date } = this.state;
+    let widget = new TimekitBooking();
+    widget.init({
+      app: "hack-reactor-124",
+      email: "EthanEFung@gmail.com",
+      apiToken: "1NFvUV1RLMYfFIKTmwOUeNBaRWMfoj01",
+      calendar: "9aefc3b5-f55b-4f41-afd2-ccb2829fdfc8",
+      availabilityView: "listing",
+      timekitFindTime: {
+        start: date,
+        future: "5 hours",
+        length: "30 minutes"
+      }
+    });
+    //find the calendar id of the shop
+    //find the date that the customer wants
+    //find the time approx that the customer wants
+    // console.log(
+    //   "APPTS: this is the date that is being processed for appt click",
+    //   // date,
+    //   time
+    // );
+
+    //send a request to timekit to find time within 3 hours of time, render 5 within 30 minutes of each other
+
+    this.setState({ openList: true }, () =>
+      console.log(
+        "this is the state once the handleFindAppt is clicked",
+        this.state
+      )
+    );
   }
 
   render() {
@@ -96,7 +128,10 @@ class Appointments extends Component {
           handleTimeChange={this.handleTimeChange}
           handleFindApptClick={this.handleFindApptClick}
         />
-        <AppointmentsList />
+        <Collapse in={this.state.openList}>
+          {/* <Button onClick={() => this.setState({ openList: false })}>x</Button> */}
+          <AppointmentsList findingAppt={this.state.openList} />
+        </Collapse>
       </div>
     );
   }
--- client/app/components/shopProfilePage/AppointmentsList.jsx
+++ client/app/components/shopProfilePage/AppointmentsList.jsx
@@ -7,23 +7,7 @@ class AppointmentsList extends Component {
     super(props);
   }
 
-  componentDidMount() {
-    let widget = new TimekitBooking();
-    let tmrw = new Date(2017, 8, 7);
-    console.log("this is tmrw", tmrw);
-    widget.init({
-      app: "hack-reactor-124",
-      email: "EthanEFung@gmail.com",
-      apiToken: "1NFvUV1RLMYfFIKTmwOUeNBaRWMfoj01",
-      calendar: "9aefc3b5-f55b-4f41-afd2-ccb2829fdfc8",
-      availabilityView: "listing",
-      timekitFindTime: {
-        start: tmrw,
-        future: "5 hours",
-        length: "1 Hour"
-      }
-    });
-  }
+  componentDidMount() {}
 
   render() {
     return <div id="bookingjs" />;
