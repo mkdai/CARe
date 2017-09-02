32543ed53e11815f9dfe76f8f95e570722d75d3d
Ethan Fung <EthanEFung@gmail.com>
2017-09-01 21:10:13 -0700

    Render beta modal for shopdashboard config testing

--- client/app/containers/shopDashboard/ShopDashboard.jsx
+++ client/app/containers/shopDashboard/ShopDashboard.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import AppointmentCalendar from "../../components/shopDashboard/AppointmentCalendar.jsx";
 import NavigationBar from "../../containers/navBar/NavigationBar";
-import { Jumbotron, Grid, Row, Col } from "react-bootstrap";
+import { Jumbotron, Grid, Row, Col, Modal, Button } from "react-bootstrap";
 
 class ShopDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+  }
+
+  handleOpenModal() {
+    console.log("click is heard for modal");
+    this.setState({ show: true });
+  }
   render() {
     return (
       <div className="container">
@@ -22,6 +34,20 @@ class ShopDashboard extends Component {
             <Col>
               <h1>I am the shopdashboard</h1>
             </Col>
+          </Row>
+          <Row>
+            <Button onClick={this.handleOpenModal}>Click</Button>
+          </Row>
+          <Row>
+            <Modal
+              show={this.state.show}
+              onHide={() => this.setState({ show: false })}
+            >
+              <Modal.Header closeButton />
+              <Modal.Body>
+                <h1>This is the modal body</h1>
+              </Modal.Body>
+            </Modal>
           </Row>
 
           <Row>
