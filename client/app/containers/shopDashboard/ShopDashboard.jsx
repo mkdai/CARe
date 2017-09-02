c1b0d942c342f5f70af2631f56de79f328b1c23e
Ethan Fung <EthanEFung@gmail.com>
2017-09-01 20:45:42 -0700

    Render beta view of shopdashboard with navigationbar

--- client/app/containers/shopDashboard/ShopDashboard.jsx
+++ client/app/containers/shopDashboard/ShopDashboard.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import AppointmentCalendar from "../../components/shopDashboard/AppointmentCalendar.jsx";
+import NavigationBar from "../../containers/navBar/NavigationBar";
+import { Jumbotron, Grid, Row, Col } from "react-bootstrap";
 
 class ShopDashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>I am the shop dashboard</h1>
-        <AppointmentCalendar />
+      <div className="container">
+        <NavigationBar />
+        <Grid fluid={true}>
+          <Row>
+            <h1 />{" "}
+          </Row>
+          <Row>
+            <h1 />{" "}
+          </Row>
+          <Row>
+            <h1 />{" "}
+          </Row>
+          <Row>
+            <Col>
+              <h1>I am the shopdashboard</h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              {"   "}
+              <AppointmentCalendar />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
