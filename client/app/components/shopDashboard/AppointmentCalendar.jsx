633ca1859d570f51fde38c8a2ac40aee404e5c5a
Ethan Fung <EthanEFung@gmail.com>
2017-09-05 11:11:13 -0700

    have psuedo code of shop apptCal expectations

--- client/app/components/shopDashboard/AppointmentCalendar.jsx
+++ client/app/components/shopDashboard/AppointmentCalendar.jsx
@@ -3,12 +3,25 @@ import { Well } from "react-bootstrap";
 import $ from "jquery";
 import axios from "axios";
 import fullCalendar from "fullcalendar";
+<<<<<<< HEAD
 
 class AppointmentCalendar extends Component {
   /* ShopDashboard Appointment Calendar should
  *  // be able to render month, week, day, and daylist views of all the shops bookings
  *     month view should simply have an indicator of appointments (not listed)
  *   be able to click each individual appointments, and see relevant info about the booking
+=======
+import timekit from "timekit-sdk";
+import {
+  timekitApp,
+  timekitEmail,
+  timekitPassword
+} from "../../../../env/config";
+
+class AppointmentCalendar extends Component {
+  /* ShopDashboard Appointment Calendar should
+ *   be able to render month, week, day, and daylist views of all the shops bookings
+>>>>>>> have psuedo code of shop apptCal expectations
  *   be able to set the week hours
  *     should have the capability to set different days
  *     should be able to set different hours
@@ -23,11 +36,24 @@ class AppointmentCalendar extends Component {
   }
 
   componentDidMount() {
+<<<<<<< HEAD
     console.log("mounting appointment calendar", this.props);
     axios
       .get(`api/shopdashboard/getCalendar`, {
         params: { id: this.props.calId }
       })
+=======
+    timekit.configure({
+      app: timekitApp,
+      inputTimestampFormat: "U",
+      outputTimestampFormat: "U"
+    });
+    // Timestamps coming and going to timekit sdk must be unicode
+
+    timekit
+      .auth({ email: timekitEmail, password: timekitPassword })
+      .then(() => timekit.include("attributes").getBookings())
+>>>>>>> have psuedo code of shop apptCal expectations
       .then(res => {
         console.log("successfully received calandar from server", res.data);
         this.setState({ bookings: res.data });
