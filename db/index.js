558c61bb3f8648593f7fcc436fa3241d7ed9db94
Kiwilicious <jhwang137@gmail.com>
2017-09-05 11:55:20 -0700

    Added car/history get methods

--- db/index.js
+++ db/index.js
@@ -20,11 +20,12 @@ const Shop = db.define("shop", {
 });
 
 const Car = db.define("car", {
+  license: { type: Sequelize.STRING, allowNull: true },
   make: { type: Sequelize.STRING, allowNull: false },
   model: { type: Sequelize.STRING, allowNull: false },
   nextService: { type: Sequelize.ARRAY(Sequelize.TEXT), allowNull: true },
   picture: { type: Sequelize.TEXT, allowNull: true },
-  vin: { type: Sequelize.STRING, allowNull: false },
+  vin: { type: Sequelize.STRING, allowNull: true },
   year: { type: Sequelize.INTEGER, allowNull: false }
 });
 
