508d3d6f7d7c587e0acab4494ece2f2f4e1cab0a
mkdai <michaelkdai@gmail.com>
2017-09-06 19:47:25 -0700

    put tabs into shop profile, made modal for review post but doenst work yet

--- client/app/components/shopProfilePage/Reviews.jsx
+++ client/app/components/shopProfilePage/Reviews.jsx
@@ -1,16 +1,71 @@
 import React, { Component } from "react";
 import ReviewEntry from "./ReviewEntry.jsx";
+import { connect } from "react-redux";
+import axios from "axios";
+import {
+  Modal,
+  Button,
+  ButtonToolbar,
+  ToggleButtonGroup,
+  ToggleButton,
+  Col
+} from "react-bootstrap";
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser.currentUser
+  };
+}
 class Reviews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({ showModal: true });
+  }
+  closeModal() {
+    this.setState({ showModal: false });
+  }
   render() {
     return (
-      <div>
-        <div>I AM REVIEWS</div>
+      <Col>
+        <Button onClick={this.openModal}>Post a Review</Button>
         {this.props.reviews.map(review => {
           return <ReviewEntry review={review} />;
         })}
-      </div>
+        <Modal show={this.state.showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <h2>Post a Review!</h2>
+          </Modal.Header>
+          <div>
+            Rating:
+            <ButtonToolbar>
+              <ToggleButtonGroup
+                type="radio"
+                name="options"
+                defaultValue={5}
+                onChange={e => console.log("hello")}
+              >
+                <ToggleButton value={1}>1</ToggleButton>
+                <ToggleButton value={2}>2</ToggleButton>
+                <ToggleButton value={3}>3</ToggleButton>
+                <ToggleButton value={4}>4</ToggleButton>
+                <ToggleButton value={5}>5</ToggleButton>
+              </ToggleButtonGroup>
+            </ButtonToolbar>
+          </div>
+          <Modal.Footer>
+            <Button>Click</Button>
+          </Modal.Footer>
+        </Modal>
+      </Col>
     );
   }
 }
 
-export default Reviews;
+export default connect(mapStateToProps)(Reviews);
