79ecb81c7d8c685215faffdb4e30af4bb27d22b9
Ethan Fung <EthanEFung@gmail.com>
2017-09-06 18:52:47 -0700

    getCalendar now renders proper calendar from timekit

--- client/app/components/shopDashboard/AppointmentCalendar.jsx
+++ client/app/components/shopDashboard/AppointmentCalendar.jsx
@@ -7,6 +7,7 @@ import fullCalendar from "fullcalendar";
 class AppointmentCalendar extends Component {
   /* ShopDashboard Appointment Calendar should
  *  // be able to render month, week, day, and daylist views of all the shops bookings
+ *     month view should simply have an indicator of appointments (not listed)
  *   be able to click each individual appointments, and see relevant info about the booking
  *   be able to set the week hours
  *     should have the capability to set different days
@@ -22,9 +23,11 @@ class AppointmentCalendar extends Component {
   }
 
   componentDidMount() {
-    console.log("appointment calendar mounts now");
+    console.log("mounting appointment calendar", this.props);
     axios
-      .get(`api/shopdashboard/getCalendar/:id`)
+      .get(`api/shopdashboard/getCalendar`, {
+        params: { id: this.props.calId }
+      })
       .then(res => {
         console.log("successfully received calandar from server", res.data);
         this.setState({ bookings: res.data });
--- server/controllers/shopDashCtrl.js
+++ server/controllers/shopDashCtrl.js
@@ -17,10 +17,10 @@ timekit.configure({
 
 module.exports = {
   getShopId: (req, res) => {
-    console.log("received request to get shop id", req.query);
+    console.log("received request to get shop id");
     User.findOne({ where: { id: req.query.userId } })
       .then(user => {
-        console.log("found user", user.dataValues.shopId);
+        console.log("found userId: ", user.dataValues.shopId);
         res.status(200).send({ shopId: user.dataValues.shopId });
       })
       .catch(err => console.log("could not find user", err));
@@ -30,11 +30,17 @@ module.exports = {
     console.log("received request to get calendar");
     timekit
       .auth({ email: timekitEmail, password: timekitPassword })
-      .then(() => timekit.include("attributes").getBookings())
+      .then(() => timekit.include("attributes", "calendar").getBookings())
       .then(books => {
         let bookings = [];
+        console.log("this is the request", req.query);
         books.data.forEach(booking => {
-          if (!booking.completed && booking.state === "confirmed") {
+          if (
+            !booking.completed &&
+            !!booking.calendar &&
+            booking.calendar.id === req.query.id &&
+            booking.state === "confirmed"
+          ) {
             let { start, end, what } = booking.attributes.event;
             let title = what;
             bookings.push({ start, end, title });
@@ -56,13 +62,11 @@ module.exports = {
           description: "testing this calendar"
         })
       )
-      .then(cal => {
-        console.log("created calendar", cal.data, "storing in database");
-        Shop.update({ calendar_id: cal.data.id }, { where: { shopId: 1 } });
-      })
-      .then(() => Shop.findAll())
-      .then(res => console.log("these are the shops", res))
-      .catch(err => res.send("could not create calendar", err));
+      .then(x => Shop.update({ calendar_id: x.data.id }, { where: req.body }))
+      .then(() => Shop.findOne({ where: req.body }))
+      .then(x => res.status(201).send({ calId: x.dataValues.calendar_id }))
+      .then(() => console.log("sent shop calendar_id to front end"))
+      .catch(err => res.status(400).send(err));
   },
   storeCalendar: (req, res) => {},
   deleteCalendar: () => {}
