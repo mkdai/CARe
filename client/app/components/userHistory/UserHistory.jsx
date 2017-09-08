432080580551c64ad08a38378cea79a16de39c4a
Kiwilicious <jhwang137@gmail.com>
2017-09-07 19:05:54 -0700

    Continue work on user reviews

--- client/app/components/userHistory/UserHistory.jsx
+++ client/app/components/userHistory/UserHistory.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { Grid, ListGroup } from "react-bootstrap";
 import UserHistoryEntry from "./UserHistoryEntry.jsx";
+import axios from "axios";
 
 export default class UserHistory extends Component {
+  constructor() {
+    super();
+  }
+
+  // componentDidMount() {
+  //   axios.get('/api/')
+  // }
+
   render() {
     return (
       <Grid>
