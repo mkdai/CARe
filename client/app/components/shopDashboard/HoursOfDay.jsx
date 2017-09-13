1888cf86e3e9e279ba7a93eb82afad6788532d54
Ethan Fung <EthanEFung@gmail.com>
2017-09-12 22:20:43 -0700

    Render dynamic start and end times for hours of Operation

--- client/app/components/shopDashboard/HoursOfDay.jsx
+++ client/app/components/shopDashboard/HoursOfDay.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TimePicker from "react-bootstrap-time-picker";
+const l = console.log;
 
 class HoursOfDay extends Component {
   constructor(props) {
@@ -9,15 +10,43 @@ class HoursOfDay extends Component {
       end: 0
     };
   }
+
+  componentDidMount() {
+    l(this.props.day);
+  }
   render() {
     return (
       <div>
         <span>{this.props.day}</span>
         <span>
-          <TimePicker step={30} onChange={this.state.start} />
+          <TimePicker
+            step={30}
+            value={this.state.start}
+            onChange={e => {
+              this.setState({ start: e }, () =>
+                this.props.handleHoursOfOpChange(
+                  this.props.day,
+                  e,
+                  this.state.end
+                )
+              );
+            }}
+          />
         </span>
         <span>
-          <TimePicker step={30} onChange={this.state.end} />
+          <TimePicker
+            step={30}
+            value={this.state.end}
+            onChange={e => {
+              this.setState({ end: e }, () =>
+                this.props.handleHoursOfOpChange(
+                  this.props.day,
+                  this.state.start,
+                  e
+                )
+              );
+            }}
+          />
         </span>
       </div>
     );
