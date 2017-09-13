3522a996333ae21742c3800c86609cd1d93fcd32
Kiwilicious <jhwang137@gmail.com>
2017-09-13 09:11:54 -0700

    Changed landing page around

--- client/app/components/landingPage/Testing.jsx
+++ client/app/components/landingPage/Testing.jsx
@@ -19,6 +19,43 @@ export default class Testing extends Component {
             </div>{" "}
           </div>
         </Parallax>
+
+        <div className="parallax-seperator">
+          Ut malesuada cursus eros, eget viverra ex semper sit amet. Phasellus
+          laoreet imperdiet urna, ac accumsan urna fringilla eget. Nulla tempus
+          est ante, sit amet rutrum nulla ullamcorper a. Phasellus commodo
+          sagittis dolor eu varius. Duis eu lacus ut lectus maximus elementum.
+          Curabitur bibendum porttitor magna non lacinia. Integer pharetra urna
+          lacus, ut convallis libero ultrices id. Maecenas at metus non massa
+          luctus laoreet. Aenean condimentum augue non neque varius, at
+          sollicitudin diam fringilla. Orci varius natoque penatibus et magnis
+          dis parturient montes, nascetur ridiculus mus. Nulla feugiat ligula
+          enim, vitae mattis dui vestibulum eu. Integer pulvinar luctus odio at
+          tempor.
+          <hr />
+          Praesent neque neque, convallis vitae imperdiet sit amet, gravida in
+          risus. Ut elementum pharetra lobortis. In vel semper lacus. Cras
+          dictum sem enim. Nam a interdum nunc, quis semper velit. Pellentesque
+          vehicula cursus felis. Proin at mi eget sapien mollis ullamcorper non
+          a felis. Nulla at mauris condimentum, ultrices tortor eu, tincidunt
+          augue. Vivamus eget tellus nisl. Morbi facilisis, diam id molestie
+          pulvinar, neque mi maximus quam, non venenatis libero lectus et dolor.
+          Duis volutpat massa ac purus tempus ornare.
+        </div>
+        <Parallax strength={300}>
+          <Background>
+            <img
+              src="https://1ifhnhasx0c1wyr6x2cjm7b8-wpengine.netdna-ssl.com/wp-content/uploads/2015/01/Made-in-office-1429_1.jpg"
+              style={{ height: "100vh", filter: "blur(3px)" }}
+            />
+          </Background>
+          <div className="parallax">
+            {" "}
+            <div className="inner">
+              <h1>We care more</h1>{" "}
+            </div>{" "}
+          </div>
+        </Parallax>
         <div className="parallax-seperator">
           <h1 className="quotes-header">Customer Testimonials</h1>
           <hr className="landing-hr" />
@@ -59,42 +96,6 @@ export default class Testing extends Component {
             </h2>
             <cite className="landing-cite">-J6K</cite>
           </blockquote>
-        </div>
-        <Parallax strength={300}>
-          <Background>
-            <img
-              src="https://1ifhnhasx0c1wyr6x2cjm7b8-wpengine.netdna-ssl.com/wp-content/uploads/2015/01/Made-in-office-1429_1.jpg"
-              style={{ height: "100vh", filter: "blur(3px)" }}
-            />
-          </Background>
-          <div className="parallax">
-            {" "}
-            <div className="inner">
-              <h1>We care more</h1>{" "}
-            </div>{" "}
-          </div>
-        </Parallax>
-        <div className="parallax-seperator">
-          Ut malesuada cursus eros, eget viverra ex semper sit amet. Phasellus
-          laoreet imperdiet urna, ac accumsan urna fringilla eget. Nulla tempus
-          est ante, sit amet rutrum nulla ullamcorper a. Phasellus commodo
-          sagittis dolor eu varius. Duis eu lacus ut lectus maximus elementum.
-          Curabitur bibendum porttitor magna non lacinia. Integer pharetra urna
-          lacus, ut convallis libero ultrices id. Maecenas at metus non massa
-          luctus laoreet. Aenean condimentum augue non neque varius, at
-          sollicitudin diam fringilla. Orci varius natoque penatibus et magnis
-          dis parturient montes, nascetur ridiculus mus. Nulla feugiat ligula
-          enim, vitae mattis dui vestibulum eu. Integer pulvinar luctus odio at
-          tempor.
-          <hr />
-          Praesent neque neque, convallis vitae imperdiet sit amet, gravida in
-          risus. Ut elementum pharetra lobortis. In vel semper lacus. Cras
-          dictum sem enim. Nam a interdum nunc, quis semper velit. Pellentesque
-          vehicula cursus felis. Proin at mi eget sapien mollis ullamcorper non
-          a felis. Nulla at mauris condimentum, ultrices tortor eu, tincidunt
-          augue. Vivamus eget tellus nisl. Morbi facilisis, diam id molestie
-          pulvinar, neque mi maximus quam, non venenatis libero lectus et dolor.
-          Duis volutpat massa ac purus tempus ornare.
         </div>
         <Parallax strength={300}>
           <Background>
