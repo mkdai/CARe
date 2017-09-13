6e074877b86c7cd9f221b28f8a244cd2c0ab916d
Ethan Fung <EthanEFung@gmail.com>
2017-09-12 19:56:28 -0700

    Developing HoursOfDay with dynamic TimePickers

--- client/app/components/shopDashboard/HoursOfDay.jsx
+++ client/app/components/shopDashboard/HoursOfDay.jsx
@@ -4,17 +4,20 @@ import TimePicker from "react-bootstrap-time-picker";
 class HoursOfDay extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      start: 0,
+      end: 0
+    };
   }
   render() {
     return (
       <div>
         <span>{this.props.day}</span>
         <span>
-          <TimePicker step={30} onChange={this.state.time} />
+          <TimePicker step={30} onChange={this.state.start} />
         </span>
         <span>
-          <TimePicker step={30} />
+          <TimePicker step={30} onChange={this.state.end} />
         </span>
       </div>
     );
