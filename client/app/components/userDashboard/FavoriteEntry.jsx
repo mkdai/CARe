d7848c61d2d2dc470983280ed188cf0b99f84d72
Michael Griffin <michaelgriffin@Michaels-MacBook-Air.local>
2017-09-13 19:03:33 -0700

    now linking favorites to shop profile

--- client/app/components/userDashboard/FavoriteEntry.jsx
+++ client/app/components/userDashboard/FavoriteEntry.jsx
@@ -10,6 +10,7 @@ import {
   Well,
   Row
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class FavoriteEntry extends React.Component {
   constructor(props) {
@@ -22,13 +23,13 @@ export default class FavoriteEntry extends React.Component {
     axios
       .get(`/api/userProfile/getShop/${this.props.shop.shopId}`)
       .then(data => {
-        console.log("SHOP IN FAVENTRY: ", data.data[0]);
+        // console.log("SHOP IN FAVENTRY: ", data.data[0]);
         this.setState({
           picture: data.data[0].picture,
           phone: data.data[0].phone,
           address: data.data[0].address,
           email: data.data[0].email,
-          name: data.data[0].yelp_id
+          name: data.data[0].name
         });
       })
       .catch(err => {
@@ -37,16 +38,22 @@ export default class FavoriteEntry extends React.Component {
   }
 
   render() {
-    console.log("PROPS IN FAVORITEENTRy: ", this.props.shop);
+    console.log("PROPS IN FAVORITEENTRy: ", this.props.shop.shop.yelp_id);
     return (
-      <Col lg={4}>
-        <div className="fav-entry">
-          <h4>{this.state.name}</h4>
-          <img src={this.state.picture} />
-          <div>{this.state.address}</div>
-          <div>Phone: {this.state.phone}</div>
-          <div>Email: {this.state.email}</div>
-        </div>
+      <Col lg={12} sm={12}>
+        <Link to={`/shops?idstring=${this.props.shop.shop.yelp_id}`}>
+          <div className="fav-entry">
+            <img className="fav-pic" src={this.state.picture} />
+            <div className="fav-info">
+              <h4>{this.state.name}</h4>
+              <div className="fav-text">
+                <div className="fav-address">{this.state.address}</div>
+                <div className="fav-phone">Phone: {this.state.phone}</div>
+                <div className="fav-email">Email: {this.state.email}</div>
+              </div>
+            </div>
+          </div>
+        </Link>
       </Col>
     );
   }
