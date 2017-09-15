9db8f9650d817e9ca2494650cdff791e1fa25ed2
mkdai <michaelkdai@gmail.com>
2017-09-15 09:24:20 -0700

    redirect uri changed to herokuapp site

--- client/Auth/auth0-variables.js
+++ client/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'kiwilicious.auth0.com',
-  clientId: 'F6xKsYH6LQgSuX9XDdmqjtZJsOtwB9Ds',
-  callbackUrl: 'http://localhost:3000/loadingpage'
+  domain: "kiwilicious.auth0.com",
+  clientId: "F6xKsYH6LQgSuX9XDdmqjtZJsOtwB9Ds",
+  callbackUrl: "https://handledwithcare.herokuapp.com/loadingpage"
 };
